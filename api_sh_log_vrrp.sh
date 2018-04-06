1c3ad4e7b6bd69dfdad4d0a48b0544a1c61bda86
y.ishibashi <36189308+yu-ishiba@users.noreply.github.com>
2018-04-06 18:00:17 +0900

    Delete api_sh_log_vrrp.sh

--- api_sh_log_vrrp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -isS -X POST http://$1:3000/rpc/ -H "Accept: application/xml" -H "Content-Type: application/xml"  -u "root:Ncinci01" -d "<get-log><filename>vrrp</filename></get-log>"
